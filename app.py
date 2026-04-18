3f0fa31ca85f9d4db26875e9dbec3516157da7f1
lianah10-lab <lianah10@uci.edu>
2026-04-18 16:25:10 -0700

    app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if tn and tn in data["trails"]:
                     font-size: 40px; 
                     margin-top: -10px;
                 ">
-                    <span style="font-size: 20px;">🤍</span> {avg_score:.1f} <span style="font-size: 16px; color: gray;">/ 5.0</span>
+                    <span style="font-size: 18px;">🤍</span> {avg_score:.1f} <span style="font-size: 16px; color: gray;">/ 5.0</span>
                 </h1>
             </div>
         """, unsafe_allow_html=True)
